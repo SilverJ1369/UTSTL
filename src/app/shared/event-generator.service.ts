29b31652333219b3201a1d88f9427b45aff63a90
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2023-12-24 16:59:04 -0600

    initial basic timeline implemented

---
+++ src/app/shared/event-generator.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventGeneratorService {
+
+  //empty array to store incoming events from backend
+  events: Event[] = [];
+
+  constructor() { }
+
+  //create variables for starting and ending date for scale
+
+}
